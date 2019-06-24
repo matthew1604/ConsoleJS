0b2b8ee2c8709e7f5b4d4202fa7ae585ddc36519
matthew1604 <vergely.matt@gmail.com>
2019-06-24 19:20:58 +0200

    v3

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var textarea = document.getElementById('textarea'),
+let textarea = document.getElementById('textarea'),
     autorunCheckbox = document.getElementById('autorun'),
     debugCheckbox = document.getElementById('debug'),
     executeButton = document.getElementById('execute'),
     clearButton = document.getElementById('clear'),
-    result = document.getElementById('result'),
-    str = textarea.value;
+    result = document.getElementById('result');
+
+function exec() {
+    let str = "try{" + textarea.value + "}catch(e){clear(); if(debugCheckbox.checked) log(e);}";
+    eval(str);
+}
 
 function log(...obj) {
     result.innerHTML = obj.toString();
@@ -17,33 +21,32 @@ function clear() {
 function autorun() {
     setTimeout(function () {
         if (textarea.value !== "") {
-            str = "try{" + textarea.value + "}catch(e){if(debugCheckbox.checked) {clear(); log(e)}}";
-            eval(str);
+            exec();
         } else clear();
     }, 100);
 }
 
-eval(str);
+exec();
 
 textarea.addEventListener("keydown", autorun);
 
 autorunCheckbox.addEventListener("click", function() {
     if (autorunCheckbox.checked) {
-        textarea.addEventListener("keydown", autorun);
-        executeButton.style.display = "none";
         log = function(...obj) { result.innerHTML = obj.toString(); };
+        textarea.addEventListener("keydown", autorun);
+        exec();
     }
     else {
-        textarea.removeEventListener("keydown", autorun);
-        executeButton.style.display = "";
         log = function(...obj) { result.innerHTML += obj.toString(); };
+        textarea.removeEventListener("keydown", autorun);
     }
 });
 
+debugCheckbox.addEventListener("click", exec);
+
 executeButton.addEventListener("click", function () {
-    str = "try{" + textarea.value + "}catch(e){clear(); log(e)}";
     clear();
-    eval(str);
+    exec();
 });
 
 clearButton.addEventListener("click",clear);
